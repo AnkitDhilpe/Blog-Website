b37c3e09996135330eba63d3b1b95ab13bc6958f
AnkitDhilpe <ankitdhilpe123@gmail.com>
2024-10-10 18:12:45 +0530

    third commit

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const connectDB = async ()=>{
     try {
         mongoose.set("strictQuery", false);
-        const conn = await mongoose.connect("mongodb+srv://ankitdhilpe123:qRr9S7qxo26ioh5P@cluster0.qey54.mongodb.net/Blog-website");
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Database Connected : ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
